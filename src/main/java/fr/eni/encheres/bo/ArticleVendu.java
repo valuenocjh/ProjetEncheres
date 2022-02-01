68cd2c46cecb7c0b08d499a30747f518f90dcd8e
hzakrajsek2021 <hzakrajsek2021@177SE44-90JBC5J.ad.campus-eni.fr>
2022-02-01 08:32:49 +0100

    add class ArticleVendu

---
+++ src/main/java/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,108 @@
+package fr.eni.encheres.bo;
+
+import java.util.Date;
+
+public class ArticleVendu {
+
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int prixInitial;
+	private int prixVente;
+	private Utilisateur utilisateur;
+	private Categorie categorie;
+	
+	
+	public ArticleVendu(){
+		
+	}
+
+
+	public ArticleVendu(int noArticle, String nomArticle, String description, Date dateDebutEncheres,
+			Date dateFinEncheres, int prixInitial, int prixVente, Utilisateur utilisateur, Categorie categorie) {
+		setNoArticle(noArticle);
+		setNomArticle(nomArticle);
+		setDescription(description);
+		setDateDebutEncheres(dateDebutEncheres);
+		setDateFinEncheres(dateFinEncheres);
+		setPrixInitial(prixInitial);
+		setPrixVente(prixVente);
+		setUtilisateur(utilisateur);
+		setCategorie(categorie);
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	public Date getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	public void setDateFinEncheres(Date dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	public int getPrixInitial() {
+		return prixInitial;
+	}
+
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
+	}
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	
+}

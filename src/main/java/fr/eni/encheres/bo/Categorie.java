75c83a19588625d24cd84aadbbcf6edce70a3d86
33788 <33788@LAPTOP-U3NJ7IID>
2022-02-01 08:31:18 +0100

    Creation Categorie

---
+++ src/main/java/fr/eni/encheres/bo/Categorie.java
@@ -0,0 +1,49 @@
+package fr.eni.encheres.bo;
+
+public class Categorie {
+
+
+	private int noCategorie;
+	private String libelle;
+	
+	
+	
+	/*
+	 * Constructeur 
+	 */
+	
+	public Categorie(String libelle) {
+		setLibelle(libelle);
+	}
+
+	/*
+	 * Constructeur
+	 */
+	
+	public Categorie() {
+		
+	}
+
+
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
+	
+}
